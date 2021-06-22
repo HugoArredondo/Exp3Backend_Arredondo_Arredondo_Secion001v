17b77a8d7cf00e769a398c2284cf1f25aad109d4
HugoArredondo <hu.arredondo@duocuc.cl>
2021-06-21 22:25:52 -0400

    creacion se superusuario admin

--- ProyectoDjango/sams/models.py
+++ ProyectoDjango/sams/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Registro(models.Model):
-    rutRegistro = models.IntegerField(primary_key=True, max_length=8, verbose_name='Rut')
+    rutRegistro = models.IntegerField(primary_key=True, verbose_name='Rut')
     dvRegistro = models.CharField(max_length=1, verbose_name='Dígito Verificador')
     nombreRegistro = models.CharField(max_length=50, verbose_name='Nombre')
     apellidoRegistro = models.CharField(max_length=50, verbose_name='Apellido')
     fechaNacimientoRegistro = models.DateField(verbose_name='Fecha de Nacimiento')
-    numContactoRegistro = models.IntegerField(max_length=8, verbose_name='Número de Contacto')
+    numContactoRegistro = models.IntegerField(verbose_name='Número de Contacto (+569)')
     correoRegistro = models.CharField(max_length=100, verbose_name='Correo Electrónico')
     nombreUsuarioRegistro = models.CharField(max_length=50, verbose_name='Nombre de Usuario')
     contraseñaRegistro = models.CharField(max_length=10, verbose_name='Contraseña')
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Exp3Backend_Arredondo_Arredondo_Secion001v
 Repositorio para la tercera entrega del proyecto web de Programación Web donde se trabaja con el framework Django
+
+Super Administrador (superuser)
+    username: admin_sams
+    password: sams_producciones
